819b38c270d28315805e12d9a090345a017715ce
Gabriel_Lewicki <127357642+Gaby-L33@users.noreply.github.com>
2023-03-23 22:59:13 -0300

    guia 3 ejercicios extras

---
+++ practica/guia 3 practica extra/src/guia/pkg3/practica/extra/ejercicio1ExtraGuia3.java
@@ -0,0 +1,31 @@
+/*
+|Dado un tiempo en minutos, calcular su equivalente en días y horas.
+|Por ejemplo, si el usuario ingresa 1600 minutos, el sistema debe calcular su equivalente: 1 día, 2 horas.
+ */
+package guia.pkg3.practica.extra;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ejercicio1ExtraGuia3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int minutos, horas, dia;
+       
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese los minutos a transformar");
+        minutos = leer.nextInt();
+        
+        if (minutos<1440){
+            
+        }
+    }
+    
+}
